731e7ff7b3154e7324529074d8110e0c8e79ff52
sunilreddy1257 <sunilreddy1257@gmail.com>
2019-02-04 22:30:39 +0530

    user creation using fireBaseAuth  and sign in done

--- FirebaseStorage/FirebaseStorage/Controllers/ViewController.swift
+++ FirebaseStorage/FirebaseStorage/Controllers/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import FirebaseAuth
+
 class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var textName: UITextField!
@@ -37,6 +39,55 @@ class ViewController: UIViewController, UITextFieldDelegate {
    
     @IBAction func submitButtonPressed(_ sender: Any) {
 
+        if segmentedControl.selectedSegmentIndex == 0 {
+
+            if textLoginEmail.text?.count == 0 || textLoginPassword.text?.count == 0
+            {
+                showingAlert(message: "Please enter all fields")
+            }else {
+                Auth.auth().signIn(withEmail: textLoginEmail.text ?? "sunil@firebase.com", password: textLoginPassword.text ?? "1234") { (authDataResults, error) in
+
+                    if error != nil {
+                        print(error)
+                        return
+                    }
+                     self.showingAlert(message: "successfully user created \(authDataResults?.user.email)")
+                }
+            }
+
+        }else {
+
+            if textName.text?.count == 0 || textPassword.text?.count == 0 || textEmail.text?.count == 0
+            {
+                showingAlert(message: "Please enter all fields")
+            } else {
+                Auth.auth().createUser(withEmail: textEmail.text ?? "sunil@firebase.com", password: textPassword.text ?? "1234") { (userDetails, error) in
+                    let userData = userDetails
+                    if error != nil {
+                        print(error)
+                        return
+                    }
+
+                    guard (userData?.user.uid) != nil else
+                    {
+                        self.showingAlert(message: "user not created")
+                        return
+                    }
+                     self.showingAlert(message: "successfully user created \(userData?.user.email)")
+
+                }
+            }
+        }
+    }
+
+    func showingAlert(message : String) {
+        let alertViewController = UIAlertController.init(title: "", message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction.init(title: "OK", style: .default) { (alert) in
+        }
+        alertViewController.addAction(alertAction)
+        self.present(alertViewController, animated: true) {
+
+        }
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
